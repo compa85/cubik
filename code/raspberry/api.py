46557114c53139a6da09cd7d204f561e76479dac
compa85 <dade.compagnoni@gmail.com>
2024-06-23 15:54:18 +0200

    Feat(raspberry): enable/disable motors api

--- code/raspberry/api.py
+++ code/raspberry/api.py
@@ -11,17 +11,31 @@ def index():
   if request.method == "GET":
     action = request.args.get('action')
 
+    # -------------------- solve --------------------
     if action == "solve":
       cubeString = utils.scanCube("raspi")
       resolution = utils.solveCube(cubeString)
+      serialUtils.write("1")
       serialUtils.write(resolution)
-      return jsonify({"message": "Solved"})
+      return jsonify({"message": f"{resolution}"})
     
+    # -------------------- move ---------------------
     elif action == "move":
       movements = request.args.get('movements')
       if len(movements) > 0:
+        serialUtils.write("1")
         serialUtils.write(movements)
         return jsonify({"message": f"{movements}"}), 200
+    
+    # ---------------- enable motors ----------------
+    elif action == "enableMotors":
+      serialUtils.write("1")
+      return jsonify({"message: Motors enabled"}), 200
+    
+    # ---------------- disable motors ---------------
+    elif action == "disableMotors":
+      serialUtils.write("2")
+      return jsonify({"message: Motors disabled"}), 200
       
     return jsonify({"message": "Connected"}), 200
 
